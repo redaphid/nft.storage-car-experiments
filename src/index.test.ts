305edbf2236f17900e29e4bfb95b6a77307543ed
redaphid <iam@aaronherres.com>
2022-01-07 21:24:19 -0700

    wow, copilot to the rescue

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,4 +3,18 @@ describe("NFTLargeFileStorage", () => {
   it("should exist", () => {
     expect(NFTLargeStorage).toBeDefined();
   });
+  describe('When given an ipfs client', () => {
+    beforeEach(() => {
+      
+    });
+    it('should be able to store and retrieve a file', async () => {
+      const ipfsClient = {
+        add: jest.fn(() => Promise.resolve({ hash: 'QmXg9Pp2ytZ14xgmQjYEiHjVjMFXzCVVEcRTWJBmLgR39V' })),
+        cat: jest.fn(() => Promise.resolve('Hello World')),
+      };
+      const nftLargeStorage = new NFTLargeStorage(ipfsClient);
+      const file = await nftLargeStorage.store('Hello World');
+      expect(file).toEqual('QmXg9Pp2ytZ14xgmQjYEiHjVjMFXzCVVEcRTWJBmLgR39V');
+    }
+  });
 });

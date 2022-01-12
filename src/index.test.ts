21a0384a3037d8f86b4e148fa02658f8ff6ed173
redaphid <iam@aaronherres.com>
2022-01-11 19:22:22 -0700

    committing a ~138M iso file for testing. This will never cause me any problems

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,11 +3,13 @@ import { NFTLargeStorage } from "./index";
 import {tmpdir} from 'os';
 import {mkdir,rmdir} from 'fs/promises'
 import {randomBytes} from 'crypto';
+import {promisify} from 'util'
+
+const timeout = promisify(setTimeout)
 const randomString = (size: number) => {  
   return randomBytes(size).toString('hex');
   
 };
-
 describe("NFTLargeFileStorage", () => {
   let datadir: string;
   it("should exist", () => {
@@ -16,19 +18,23 @@ describe("NFTLargeFileStorage", () => {
   describe("Given an ipfs client", () => {    
     let ipfsClient;
     let nftLargeStorage: NFTLargeStorage;    
+    
     beforeAll(async () => {
       datadir = `${tmpdir()}/ipfs-nft-large-storage-test/${randomString(10).toString()}`;
       await mkdir(datadir, {recursive: true});
-      console.log({datadir})
-      ipfsClient = await createIpfsClient({"path":datadir});
-      await ipfsClient.config.profiles.apply("test")
+    })
 
+    beforeAll(async () => {      
+      ipfsClient = await createIpfsClient({"path":datadir});      
       nftLargeStorage = new NFTLargeStorage(ipfsClient);      
+      await nftLargeStorage.start()
     });
 
     it("should be able to store a file, and retrieve it's dag", async () => {
+      const filePath = process.env.FILE_TO_UPLOAD_PATH
+      if(!filePath) throw new Error("FILE_TO_UPLOAD_PATH env variable is not set")
 
-      const file = await nftLargeStorage.add("Hello World");
+      const file = await nftLargeStorage.add(filePath);
       console.log({ file });
       expect(file.cid.toString()).toEqual(
         "QmUXTtySmd7LD4p6RG6rZW6RuUuPZXTtNMmRQ6DSQo3aMw"
@@ -44,6 +50,7 @@ describe("NFTLargeFileStorage", () => {
     });
     afterAll(async () => {
       await ipfsClient.stop();
+      await timeout(1000)
       await rmdir(datadir, {recursive: true});
     })
   });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
-import {urlSource} from 'ipfs'
-const URL_TO_ADD = "https://mirror.math.princeton.edu/pub/ubuntu-iso/focal/ubuntu-20.04.3-desktop-amd64.iso"
+import { open as openFile } from "fs/promises";
 export class NFTLargeStorage {
-  constructor(private ipfsClient: any) {
-
-  }
-  add =async (data: string) =>{ 
-    return this.ipfsClient.add(urlSource(URL_TO_ADD), {
-      progress: (prog: any) => {
-        console.log(`received: ${prog.received}`)
+  constructor(private ipfsClient: any) {}
+  start = async () => {
+    await this.ipfsClient.config.profiles.apply("test");
+  };
+  add = async (filePath: string) => {
+    const file = await openFile(filePath, "r");
+    const stream = await file.createReadStream();
+    const result = await this.ipfsClient.add(
+      {
+        path: filePath,
+        content: stream,
+      },
+      {
+        chunker: "size-100000",
+        progress: console.log,
       }
-    }); 
+    );
+    console.log({ result });
+    return result;
     // return Promise.resolve({hash: "QmXg9Pp2ytZ14xgmQjYEiHjVjMFXzCVVEcRTWJBmLgR39V"})
-  }
+  };
 }
